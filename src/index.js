7b51f001afac03e2c647e729fc2a604841ce0739
KsenNeko <70853791+KsenNeko@users.noreply.github.com>
2020-10-03 11:26:55 +0400

    feat: add min. max, avg

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+function min() {
+  if (arguments.length > 0) {
+    let minimum = arguments[0];
+    for (let i = 0; i < arguments.length; i++) {
+      if (minimum > arguments[i]) {
+        minimum = arguments[i];
+      }
+    }
+    return minimum;
+  } else return 0;
+}
+
+function max() {
+  if (arguments.length > 0) {
+    let maximum = arguments[0];
+    for (let i = 0; i < arguments.length; i++) {
+      if (maximum < arguments[i]) {
+        maximum = arguments[i];
+      }
+    }
+    return maximum;
+  } else return 0;
+}
+
+function avg() {
+  if (arguments.length > 0) {
+    let average = 0;
+    for (let i = 0; i < arguments.length; i++) {
+      average += arguments[i];
+    }
+    return average / arguments.length;
+  } else return 0;
+}
+
 
 exports.min = function min (array) {
   return 0;
