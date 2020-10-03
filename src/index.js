5ebc22d4c590774a7a5561ff42b829ccbe26c221
KsenNeko <70853791+KsenNeko@users.noreply.github.com>
2020-10-03 11:49:33 +0400

    fix: array transfer

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-function min(arr) {
-  if (arr.length > 0) {
-    let minimum = arr[0];
-    for (let i = 0; i < arr.length; i++) {
-      if (minimum > arr[i]) {
-        minimum = arr[i];
+function min(array) {
+  if (array.length > 0) {
+    let minimum = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (minimum > array[i]) {
+        minimum = array[i];
       }
     }
     return minimum;
   } else return 0;
 }
 
-function max(arr) {
-  if (arr.length > 0) {
-    let maximum = arr[0];
-    for (let i = 0; i < arr.length; i++) {
-      if (maximum < arr[i]) {
-        maximum = arr[i];
+function max(array) {
+  if (array.length > 0) {
+    let maximum = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (maximum < array[i]) {
+        maximum = array[i];
       }
     }
     return maximum;
   } else return 0;
 }
 
-function avg(arr) {
-  if (arr.length > 0) {
+function avg(array) {
+  if (array.length > 0) {
     let average = 0;
-    for (let i = 0; i < arr.length; i++) {
-      average += arr[i];
+    for (let i = 0; i < array.length; i++) {
+      average += array[i];
     }
-    return average / arr.length;
+    return average / array.length;
   } else return 0;
 }
 
